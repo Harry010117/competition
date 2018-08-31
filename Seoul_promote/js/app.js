3654769ef96e0f2bb1e00aabb1281e563bfcff33
Harry <harry010117@gmail.com>
2018-08-31 11:38:25 +0900

    asdf

---
+++ Seoul_promote/js/app.js
@@ -0,0 +1,47 @@
+var count = 0
+var scrollEvent = false
+const sh = $(window).height()
+
+function loadOn () {
+	$("html, body").scrollTop(sh*count)
+	$(".bullet:first-child").css({background:"#6789ca"})
+	$(".bullet > .name").css({color:"#dfdfdd"})
+}
+
+function mouseScrollAnimation (e) {
+	e.preventDefault()
+	var delta = e.originalEvent.wheelDelta
+	if (delta == -120 && scrollEvent == false && count < 4) {
+		scrollEvent = true
+		count++
+	} else if (delta == 120 && scrollEvent == false && count >= 1) {
+		scrollEvent = true
+		count--
+	}
+	$("html, body").stop().animate({scrollTop:sh*count},
+		{duration:100, complete: () => {
+			scrollEvent = false
+		}
+	})
+	$(".bullet").css({background:"#dfdfdd"})
+	$(".bullet").eq(count).css({background:"#6789ca"})
+	if(count == 0) $(".bullet > .name").css({color:"#dfdfdd"})
+	else $(".bullet > .name").css({color:"#504e49"})
+}
+
+function clickScrollAnimation () {
+	count = $(this).index()
+	$("html, body").stop().animate({scrollTop:sh*count},
+		{duration:100, complete: () => {
+			scrollEvent = false
+		}
+	})
+	$(".bullet").css({background:"#dfdfdd"})
+	$(".bullet").eq(count).css({background:"#6789ca"})
+	if(count == 0) $(".bullet > .name").css({color:"#dfdfdd"})
+	else $(".bullet > .name").css({color:"#504e49"})
+}
+
+$(loadOn)
+.on("click",".bullet",clickScrollAnimation)
+.on("mousewheel","html, body",mouseScrollAnimation)
